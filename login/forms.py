b20a0783992fc8837201b4f5aa0b5a550d0426eb
Kyuubang <bayhaqisptr04@gmail.com>
2021-05-11 10:26:13 +0700

    add todo

--- login/forms.py
+++ login/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 
 class NameForm(forms.Form):
+        # TODO: Create Register form included (Username, First name, Last name, Email, Password)
         username = forms.CharField(label='Username', max_length=100)
         email = forms.CharField(label='Username', max_length=100)
         firstname = forms.CharField(label='First Name', max_length=100)
         lastname = forms.CharField(label='Last Name', max_length=100)
         password = forms.CharField(label='Password', max_length=100)
+
+        # TODO: Create Login form included (Username/Email, Password)
         

12137bc7c801a8859e4c6ce7348e30d0df0319e9
Kyuubang <bayhaqisptr04@gmail.com>
2021-05-15 16:08:55 +0700

    add specify more fields

--- login/forms.py
+++ login/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+# specify more field for user registration form
 class UserRegister(UserCreationForm):
     class Meta:
         model = User
